8ec4dfbe9a8fd4f8d0a218fe8888319367c84805
ivan.garcia <ivan.garcia@Ivan>
2023-03-02 08:12:34 +0100

    Añadir un nuevo parámetro al método operativa_cuenta, de nombre cantidad y de tipo float. Esa cantidad será la utilizada en la creación de la cuenta.

--- Cuentas/src/cuentas/Main.java
+++ Cuentas/src/cuentas/Main.java
@@ -10,10 +10,10 @@ public class Main {
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta1, 0);
     }
 
-	private static void operativa_cuenta(CCuenta cuenta1) {
+	private static void operativa_cuenta(CCuenta cuenta1, float cantidad) {
 		try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
