c04df08bca1dd394e7b46e60676096837ce55538
ivan.garcia <ivan.garcia@Ivan>
2023-03-02 08:29:51 +0100

    Insertar comentarios JavaDoc en la clase CCuenta.

--- Cuentas/src/cuentas/CCuenta.java
+++ Cuentas/src/cuentas/CCuenta.java
@@ -1,17 +1,37 @@
 package cuentas;
 
+
+
+/**
+ * Esta clase define los atributos y los metodos de la CCuenta
+ * @author ivan.garcia
+ * @version 1.0
+ */
 public class CCuenta {
 
+	
 
     private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
 
+    /**
+     *  Constructor vacio de CCuenta
+     */
     public CCuenta()
+    
+    
     {
     }
 
+    /**
+     * Constructor con valores de CCuenta
+     * @param nom - Nombre que le asignamos a la cuenta
+     * @param cue - Nº que le asignamos a la cuenta
+     * @param sal - Saldo que le asignamos a la cuenta
+     * @param tipo - Nº de Tipo de interés de la cuena
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
@@ -19,11 +39,20 @@ public class CCuenta {
         setSaldo(sal);
     }
 
+    /**
+     * Metado estado
+     * @return - Devuelve el saldo actual de la cuenta
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Método para ingresar saldo en la cuenta
+     * @param cantidad - Cantidad que queremos ingresar en la cuenta
+     * @throws Exception - Control de excepciones por si se intenta ingresar saldo en negativo.
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -31,6 +60,11 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Metodo para retirar saldo de la cuenta
+     * @param cantidad - Cantidad que deseamos retirar de la cuenta
+     * @throws Exception - Control de excepciones por si no hay saldo en cuenta
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -40,6 +74,9 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+	/**
+	 * Metodos getters/setters para acceso a los atributos de la clase
+	 */
 	private String getNombre() {
 		return nombre;
 	}
